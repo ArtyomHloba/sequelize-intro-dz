6529c7b30867e43df492b9ba62cac7c86821a333
ArtyomHloba <artemgloba2@gmail.com>
2024-10-08 12:30:34 +0300

    Add allPhones func

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ const { sequelize, Phone } = require('./models');
 //   .then(() => console.log('Sync OK'))
 //   .catch(err => console.log(err));
 
+// (async function () {
+//   try {
+//     const phone = {
+//       model: '14',
+//       brand: 'Iphone',
+//       year: 2023,
+//       ram: 6,
+//       processor: 'A14',
+//       screen: 6.1,
+//       nfc: true,
+//     };
+
+//     const createPhone = await Phone.create(phone);
+//     console.log(createPhone);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
 (async function () {
   try {
-    const phone = {
-      model: '14',
-      brand: 'Iphone',
-      year: 2023,
-      ram: 6,
-      processor: 'A14',
-      screen: 6.1,
-      nfc: true,
-    };
-
-    const createPhone = await Phone.create(phone);
-    console.log(createPhone);
+    const allPhones = await Phone.findAll();
+    console.log(allPhones);
   } catch (err) {
     console.log(err);
   }
