6dbf19b821941e5e084a1d29d1253132becd764e
ArtyomHloba <artemgloba2@gmail.com>
2024-10-08 10:48:46 +0300

    Refactor files with prettier

--- models/phone.js
+++ models/phone.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Phone extends Model {
     /**
@@ -9,21 +7,24 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   }
-  Phone.init({
-    model: DataTypes.STRING,
-    brand: DataTypes.STRING,
-    year: DataTypes.INTEGER,
-    ram: DataTypes.INTEGER,
-    processor: DataTypes.STRING,
-    screen: DataTypes.FLOAT,
-    nfc: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Phone',
-  });
+  Phone.init(
+    {
+      model: DataTypes.STRING,
+      brand: DataTypes.STRING,
+      year: DataTypes.INTEGER,
+      ram: DataTypes.INTEGER,
+      processor: DataTypes.STRING,
+      screen: DataTypes.FLOAT,
+      nfc: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: 'Phone',
+    }
+  );
   return Phone;
 };
