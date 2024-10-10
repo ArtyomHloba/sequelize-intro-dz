11e92272b13c93d7883f6ce2863394cb52faa20f
ArtyomHloba <artemgloba2@gmail.com>
2024-10-10 11:51:10 +0300

    Add some querieses for Phone database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { where, Op } = require('sequelize');
 const { sequelize, Phone } = require('./models');
 
 // sequelize
@@ -5,14 +6,14 @@ const { sequelize, Phone } = require('./models');
 //   .then(() => console.log('Sync OK'))
 //   .catch(err => console.log(err));
 
-// додавання нового телефону,
+// додавання нового телефону
 // (async function () {
 //   try {
 //     const phone = {
-//       model: 'Moto G Stylus',
+//       model: 'Xiaomi Note 11 pro',
 //       brand: 'Motorola',
-//       year: 2020,
-//       ram: 4,
+//       year: 2024,
+//       ram: 6,
 //       processor: 'Snapdragon 665',
 //       screen: 6.4,
 //       nfc: false,
@@ -25,11 +26,98 @@ const { sequelize, Phone } = require('./models');
 //   }
 // })();
 
-(async function () {
-  try {
-    const allPhones = await Phone.findAll({ raw: true });
-    console.log(allPhones);
-  } catch (err) {
-    console.log(err);
-  }
-})();
+//отримання списку телефонів
+// (async function () {
+//   try {
+//     const allPhones = await Phone.findAll({ raw: true });
+//     console.log(allPhones);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+// отримання списку телефонів (* 3-я сторінка при перегляді по 4 телефони на сторінці, упорядкованих за роком виробництва),
+// (async function () {
+//   try {
+//     const foundPhones = await Phone.findAll({
+//       raw: true,
+//       order: [['year', 'ASC']],
+//       limit: 3,
+//       offset: 4,
+//     });
+//     console.log(foundPhones);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+// отримання списку телефонів поточного року видання,
+// (async function () {
+//   try {
+//     const phones = await Phone.findAll({
+//       raw: true,
+//       where: { year: '2024' },
+//     });
+
+//     console.log(phones);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+// отримання списку телефонів старше 2022 року випуску,
+// (async function () {
+//   try {
+//     const phones = await Phone.findAll({
+//       where: { year: { [Op.lt]: 2022 } },
+//       raw: true,
+//     });
+
+//     console.log(phones);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// оновлення: змінити розмір оперативної пам'яті телефону з id: 1,
+// (async function () {
+//   try {
+//     const updated = await Phone.update(
+//       { ram: 10 },
+//       { where: { id: 1 }, raw: true, returning: true }
+//     );
+//     console.log(updated);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })();
+
+// додати NFC всім телефонам 2023 року випуску
+// (async function () {
+//   try {
+//     const phones = await Phone.update({ nfc: true }, { where: { year: 2023 } });
+//     console.log(phones);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// видалення телефону з id: 2.
+// (async function () {
+//   try {
+//     const phones = await Phone.destroy({ where: { id: 2 } });
+//     console.log(phones);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// видалення телефонів 2015 року випуску.
+// (async function () {
+//   try {
+//     const phones = await Phone.destroy({ where: { year: '2015' } });
+//     console.log(phones);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
